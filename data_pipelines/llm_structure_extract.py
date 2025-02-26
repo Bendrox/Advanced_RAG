13e3677598b68c95843eba0df19417c79d26707a
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-26 12:19:22 +0100

    Scripting

---
+++ data_pipelines/llm_structure_extract.py
@@ -0,0 +1,24 @@
+
+def extract_structure_with_llm(model,document):
+    prompt_structure_extract_json = f""" 
+            You are an expert data scientist and data engineer with an expertise in law .
+            That means that you have a deep knowlage of law.
+            
+            To do: 
+            - Extract the whole structure of this document to prepare to make the best and most reliable embedding.
+            - The output is a tabular Json. 
+            Example de contenu a mettre en accollade : 
+            'article 1er', 'paragraphe n°3', 'contenu': "3. Aux fins de la présente directive, sont 
+            criminelle, dans le but de dissimuler ou de déguiser l'origine illicite de ces biens ...".
+            
+            Rules: 
+            - Be as acurate as possible.
+            - Do not ommit information.
+            - You will be starting from the "Article"
+            - Do not omit the structure under "Article" until the last "Article".
+            - Do not translate the text : {document} 
+            
+    """
+    for chunk in model.stream(prompt_structure_extract_json):
+        print(chunk.content, end="", flush=True) 
+        result = chunk.content
---
+++ legal_tools/regul_change_analysis.py
@@ -0,0 +1,57 @@
+from data_pipelines.str_diff_tools import comparer_phrases
+from models.llm_models import llm_4o
+
+
+def get_eu_data_3p(llm_model, reglement_ue, fr_old_version, fr_new_version):
+    prompt_LCQA_3p = f""" 
+        Tu es un juriste expert avec au moins 10 ans d expérience.
+        Tu as une double expertise a la fois dans la réglementation francaise et dans les textes européens.
+        Tu as travaillé a plusieurs reprises sur la transposition des textes européens dans la réglementation francaise.
+        
+        Ta tâche :
+        -   Analyser les changements réglementaires dans la loi francaise avec pour objectif de trouver la ou les parties
+        de la directive UE (article ou articles) a l origine de ce(s) changement(s).
+        
+        Régles : 
+        - Tu te baseras uniquement sur les informations fournies pour faire ton analyse.
+        - Tu feras ton analyse de la facon la plus conconcieuse et précise 
+        - Tu ne pas pas reciter le contenu de la loi francaise avant et après modification
+        
+        Voici le text européen {reglement_ue}
+        Voici le texte de loi francais dans sa version avant l'entree en vigueur du text européen: {fr_old_version}
+        Voici le texte de loi francais dans sa version après l'entree en vigueur du text européen: {fr_new_version}
+"""
+    for chunk in llm_model.stream(prompt_LCQA_3p):
+        print(chunk.content, end="", flush=True)
+
+
+def get_eu_data_4p(llm_model, reglement_ue, fr_old_version, fr_new_version):
+    changement_derter= comparer_phrases(fr_old_version, fr_new_version)
+    print(changement_derter)
+    prompt_LCQA_4p = f""" 
+            Tu es un juriste expert avec au moins 10 ans d expérience.
+            Tu as une double expertise a la fois dans la réglementation francaise et dans les textes européens.
+            Tu as travaillé a plusieurs reprises sur la transposition des textes européens dans la réglementation francaise.
+            
+            
+            Ta tâche :
+            -   Analyser les changements réglementaires dans la loi francaise et de trouver la ou les parties
+            de la directive UE (article ou articles) a l origine de ce changement réglementaire.
+            
+            Régles : 
+            - Tu te baseras uniquement sur les informations fournies pour faire ton analyse.
+            - Tu feras ton analyse de la facon la plus conconcieuse et précise 
+            
+            Voici le text européen {reglement_ue}
+            Voici le texte de loi francais dans sa version avant l'entree en vigueur du text européen: {fr_old_version}
+            Voici le texte de loi francais dans sa version après l'entree en vigueur du text européen: {fr_new_version}
+            Voici une indication des changements entre les deux version du texte francais sur la quelle tu t appuieras pour
+            trouver la partie du texte europeen a l origine de ce changement : {changement_derter}
+
+    """
+    for chunk in llm_model.stream(prompt_LCQA_4p):
+        print(chunk.content, end="", flush=True)
+        
+                    
+        
+        

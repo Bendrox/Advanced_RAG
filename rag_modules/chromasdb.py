0ccf5ab9bc12f075167ce6982498a1f8d9d05839
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-26 15:11:43 +0100

    Scripting

--- rag_modules/chromasdb.py
+++ rag_modules/chromasdb.py
@@ -1,9 +1,13 @@
 from langchain.vectorstores import Chroma
 
-def input_data_chromasdb(chunks, embedding_model, chemin,nom_source):
-    vectordb = Chroma.from_texts( 
+def input_data_chromasdb(namedb, chunks,nom_source, embedding_model, chemin):
+    namedb = Chroma.from_texts( 
         texts=chunks,
         metadatas= [{"source": nom_source} for _ in chunks],
         persist_directory=chemin,
         embedding=embedding_model)
 
+def qa_chroma(namedb,question, k_numer ,source_filter):
+    docs = namedb.similarity_search(question,k_numer, filter={"source":source_filter})
+    
+        

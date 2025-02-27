36450e24353c30c80041c4714eb7ac56c28db742
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-27 10:22:49 +0100

    Enhance function

--- data_pipelines/url_extractor.py
+++ data_pipelines/url_extractor.py
@@ -4,8 +4,20 @@ import requests
 
 app = FirecrawlApp(api_key=os.getenv('api_key_firecraw_2'))
 
-def firecrawl_extractor(url:str):
-    "Using firecrawl to extract data from URL. Data retreived in two format (HMTL + Markdown)"
+def firecrawl_extractor_mrkd(url:str):
+    """
+    Utilise l'API Firecrawl pour extraire le contenu d'une URL donnée.  
+    Les données sont récupérées aux formats HTML et Markdown,  
+    mais seule la version Markdown est renvoyée.  
+      
+    Args: url (str): L'URL de la page web à extraire.  
+      
+    Returns: str Contenu de la page au format Markdown.  
+      
+    Remarque :  
+        La requête est effectuée en spécifiant le pays 'FR' pour  
+        adapter le contexte géographique si nécessaire.  
+    """
     scrape_result = app.scrape_url(
         url,
         params={
@@ -15,12 +27,13 @@ def firecrawl_extractor(url:str):
             }
         }
     )
-    result_mark= scrape_result['markdown']
-    result_html= scrape_result['html']
+    scrape_result = scrape_result['markdown']
+    return scrape_result
+
     
 
-def url_to_pdf(url, save_path):
-    "Transform URL to save a local file PDF"
-    response = requests.get(url)
+def url_to_local_pdf(eurlex_url, save_path):
+    "Transform local URL to save a local file PDF"
+    response = requests.get(eurlex_url)
     with open(save_path, "wb") as f:
         f.write(response.content)
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from data_pipelines
+from data_pipelines.url_extractor import url_to_local_pdf, firecrawl_extractor_mrkd
+
 
 url_aml_5="http://publications.europa.eu/resource/celex/32015L0849"
 url_crr="http://publications.europa.eu/resource/celex/32013R0575"
 
-url_to_pdf("https://eur-lex.europa.eu/legal-content/FR/TXT/PDF/?uri=CELEX:32018L0843", 
+url_to_local_pdf("https://eur-lex.europa.eu/legal-content/FR/TXT/PDF/?uri=CELEX:32018L0843", 
                 "/Users/oussa/Desktop/Github_perso/Advanced_RAG/data_input/AML5.pdf")
+
+
+
+# Step 1: inject URL for Firecrawl
+url_aml_5="http://publications.europa.eu/resource/celex/32015L0849"
+url_crr="http://publications.europa.eu/resource/celex/32013R0575"
+
+# step 2: scraping data with firecrawl  
+scrape_result = firecrawl_extractor_mrkd(url_aml_5)
+
+# step 3:
+scrape_result['Markdown']
+
+# step 4:
+
+# step 5:
+
+# step 6:
+
+# step 7:

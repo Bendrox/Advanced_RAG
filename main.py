3dcdfef234c4158ba6c01cc4b4885f34a3814f45
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-27 12:09:08 +0100

    Etape 5

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ from data_pipelines.data_cleaning import (supr_avant_directive_mrk, supr_apres_d
                                           nettoyer_markdown, weird_carac_remove, 
                                           supr_avant_reglement_mrk, supr_apres_reglement_mrk)
 
+from llm_tools.lcqa import get_eu_data_4p, get_eu_data_3p
 
 # Step 1: inject URL for Firecrawl
-print("Début de l'étape 1")
+print("Début de l'étape 1: injection des liens")
 url_aml_5="http://publications.europa.eu/resource/celex/32015L0849"
 url_crr="http://publications.europa.eu/resource/celex/32013R0575"
 print("Etape 1 terminée")
 
 # step 2: scraping data with firecrawl
-print("Début de l'étape 2: récupérer les données avec firecrawl ")
+print("Début de l'étape 2: récupération des données avec firecrawl ")
 scrape_result = firecrawl_extractor_mrkd(url_aml_5)
 print("Etape 2 terminée")
 
 # step 3: calcul nombre de token de l'extraction brute 
-print("Début de l'étape 3: calcul nombre de token de l'extraction brute")
+print("Début de l'étape 3: calcul du nombre de token de l'extraction brute")
 print(f"Le nombre de count_tokens du texte brute est de : {count_tokens(scrape_result)}")
 print("Etape 3 terminée")
 
@@ -30,8 +31,14 @@ scrape_result_3= nettoyer_markdown(scrape_result_2)
 scrape_result_4= nettoyer_markdown(scrape_result_3)
 scrape_result_5= weird_carac_remove(scrape_result_4)
 print("Succès de l'étape 4: Netoyage du text brut (en 5 étapes)")
+print(f"Nombre de token après nettoyage du text: {count_tokens(scrape_result_5)}")
+diff_nettoyage= count_tokens(scrape_result) - count_tokens(scrape_result_5)
+print(f"Nombre de token supprimés grace au nettoyage : {diff_nettoyage}  ")
 
-# step 5:
+# step 5: Option 1: LCQA - Long context Question answering 
+lcqa_3p_res= get_eu_data_3p()
+
+lcqa_4p_res=get_eu_data_4p()
 
 # step 6:
 

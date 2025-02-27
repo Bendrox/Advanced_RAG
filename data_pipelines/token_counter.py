354a12f67647853e72de8500865bda1a04c8253f
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-27 10:43:49 +0100

    Step 3

--- data_pipelines/token_counter.py
+++ data_pipelines/token_counter.py
@@ -1,6 +1,20 @@
 import tiktoken
 
 def count_tokens(text: str, model: str = "gpt-4o") -> int:
+    """
+    Calcule et affiche le nombre de tokens d'un texte donné en utilisant  
+    l'encodage spécifique au modèle OpenAI.  
+
+    Args:  
+        text (str): Le texte dont on souhaite compter les tokens.  
+        model (str, optional): Le nom du modèle pour lequel l'encodage est choisi.  
+                               Par défaut 'gpt-4o'.  
+
+    Returns: Le nombre de tokens du texte.  
+
+    Remarque : La fonction affiche également le nombre de tokens dans la console.  
+    """
     encoding = tiktoken.encoding_for_model(model)
     tokens = encoding.encode(text)
-    return len(tokens)
+    token_count = len(tokens)
+    return token_count
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data_pipelines.url_extractor import url_to_local_pdf, firecrawl_extractor_mrkd
-
+from data_pipelines.token_counter import count_tokens
 
 url_aml_5="http://publications.europa.eu/resource/celex/32015L0849"
 url_crr="http://publications.europa.eu/resource/celex/32013R0575"
@@ -16,8 +16,8 @@ url_crr="http://publications.europa.eu/resource/celex/32013R0575"
 # step 2: scraping data with firecrawl  
 scrape_result = firecrawl_extractor_mrkd(url_aml_5)
 
-# step 3:
-scrape_result['Markdown']
+# step 3: calcul nombre de token de l'extraction brute 
+print(f"Le nombre de count_tokens du texte brute est de : {count_tokens(scrape_result)}")
 
 # step 4:
 

a5b99a466332f47629c042a5686bcc48f821365e
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-27 11:03:23 +0100

    Enhancing code with followup messages

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from data_pipelines.url_extractor import url_to_local_pdf, firecrawl_extractor_mrkd
 from data_pipelines.token_counter import count_tokens
 
-url_aml_5="http://publications.europa.eu/resource/celex/32015L0849"
-url_crr="http://publications.europa.eu/resource/celex/32013R0575"
-
-url_to_local_pdf("https://eur-lex.europa.eu/legal-content/FR/TXT/PDF/?uri=CELEX:32018L0843", 
-                "/Users/oussa/Desktop/Github_perso/Advanced_RAG/data_input/AML5.pdf")
-
-
 
 # Step 1: inject URL for Firecrawl
+print("Début de l'étape 1")
+
 url_aml_5="http://publications.europa.eu/resource/celex/32015L0849"
 url_crr="http://publications.europa.eu/resource/celex/32013R0575"
 
-# step 2: scraping data with firecrawl  
+print("Etape 1 terminée")
+
+# step 2: scraping data with firecrawl
+print("Début de l'étape 2: récupérer les données avec firecrawl ")
+
 scrape_result = firecrawl_extractor_mrkd(url_aml_5)
 
+print("Etape 2 terminée")
+
 # step 3: calcul nombre de token de l'extraction brute 
+print("Début de l'étape 3: calcul nombre de token de l'extraction brute")
+
 print(f"Le nombre de count_tokens du texte brute est de : {count_tokens(scrape_result)}")
 
-# step 4:
+print("Etape 3 terminée")
+
+# step 4: Netoyage du text brut 
+print("Début de l'étape 4: Netoyage du text brut")
+
 
 # step 5:
 

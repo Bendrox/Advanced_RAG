23348b2d8190d1ad0f37811bc20f9593c75828c4
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-28 11:43:34 +0100

    Chunker config

---
+++ data_pipelines/chunker.py
@@ -0,0 +1,21 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# attention RecursiveCharacterTextSplitter	Si le « morceau » résultant dépasse chunk_size, 
+# il redescend au séparateur suivant (plus petit).
+
+
+def chunker_optimal(input_data_to_chunk):
+    """Optimal chunker for AML5 + CRR
+
+    Args:
+        input_data_to_chunk (_type_): _description_
+
+    Returns:
+        list: of chunks
+    """
+    article_splitter = RecursiveCharacterTextSplitter(
+    separators=   ["Article"],
+    chunk_size= 100, 
+    chunk_overlap=0)
+    chunks= article_splitter.split_text(input_data_to_chunk)
+    return chunks
